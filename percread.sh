5c97ee30a7be9cbc5c294c8a50ec7816a470ad7f
Dmitry Kaganovich <kaganovichd@gmail.com>
2016-10-23 11:04:28 +0300

    minor fixes in percread.sh

--- percread.sh
+++ percread.sh
@@ -3,16 +3,16 @@
 function _percread() {
 	local path="$1"
 	local rec
-	if ! rec=$(lsof -oo0 "$path" 2>/dev/null); then # -o show offset -o0 in decimal format
+	if ! rec=$(lsof -oo0 -F o "$path" 2>/dev/null); then # -o show offset -o0 in decimal format
 		echo "Failed to read file offset data: file was closed"
 		return 1
 	fi
-	local file_offset=$(cut -c3- <(echo "$rec" | sed -n 2p | awk '{print $7}')) # cut off 0t decimal prefix
-	if ! rec=$(lsof -s -o0 "$path" 2>/dev/null); then # -s show size
+	local file_offset=$(cut -c4- <(echo "$rec" | sed -n 2p)) # cut off 0t decimal prefix
+	if ! rec=$(lsof -s -F s "$path" 2>/dev/null); then # -s show size
 		echo "Failed to read file size data: file was closed"
 		return 1
 	fi
-	local file_size=$(echo "$rec" | sed -n 2p | awk '{print $7}')
+	local file_size=$(cut -c2- <(echo "$rec" | sed -n 2p))
 	local file_perc=$(awk "BEGIN {printf \"%6.2f\", 100.0 * ${file_offset} / ${file_size}}")
 	echo "$file_perc"
 	return 0
@@ -30,7 +30,7 @@ function percread() {
 	fi
 	echo "Please wait. It may take a while..."
 	# local path=$(lsof | awk -v pattern="$path_regex" '$0 ~ pattern {print $9}') # ERE
-	local path=$(lsof | awk '{print $9}' | grep "$path_regex") # BRE
+	local path=$(lsof | awk '{if(NR>1){print $9}}' | grep "$path_regex") # BRE
 	if [ -z "$path" ]; then
 		echo "Illegal argument: regex does not match any files"
 		return 1
